d1a00895f274bfa9780bcc8d0dbc403c0153830e
amefure <shachi10969@gmail.com>
2025-06-05 20:38:55 +0900

    SPMデモライブラリのデモクラス実装

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,23 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+// デフォルトではinternalのため別モジュールからはアクセスできない
+class TestLibrary {
+    var text = "Hello, World!"
+}
+
+// 別のモジュールからアクセスできるようにしたい場合はpublicを付与する
+public class TestPublicLibrary {
+    // デフォルトではinternalのため別モジュールからはアクセスできない
+    var text = "Hello, World!"
+    
+    // イニシャライザにもpublicを付与しないと別のモジュールからインスタンス化できない
+    public init(text: String = "Hello, World!") {
+        self.text = text
+    }
+    
+    // publicを指定しているので別モジュールからアクセス可能
+    public func sayHello() {
+        print(text)
+    }
+}
